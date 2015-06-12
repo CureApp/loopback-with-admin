9681090a9e3a239ce69c7ce77dbdb1a518bb5c62
shinout <shinout310@gmail.com>
2015-06-12 15:52:23 +0900

    server.js can accept callback

--- server/server.js
+++ server/server.js
@@ -3,20 +3,23 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    console.log('Web server listening at: %s', app.get('url'));
+app.start = function(callback) {
+  boot(app, __dirname, function(err) {
+    if (err) throw err;
+
+      // start the web server
+      app.listen(function() {
+        app.emit('started');
+        console.log('Web server listening at: %s', app.get('url'));
+        if (callback) callback();
+      });
   });
 };
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
 
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
-});
+// start the server if `$ node server.js`
+if (require.main === module) {
+  app.start();
+}
