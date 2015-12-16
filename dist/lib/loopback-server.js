9a9119b2d45e8ced524db33b4ce21628e3724471
CircleCI <circleci@cureapp.jp>
2015-12-16 14:23:40 +0000

    v1.7.3

---
+++ dist/lib/loopback-server.js
@@ -0,0 +1,31 @@
+var LoopbackServer, normalize;
+
+normalize = require('path').normalize;
+
+require('es6-promise').polyfill();
+
+
+/**
+launches loopback server
+
+@class LoopbackServer
+ */
+
+LoopbackServer = (function() {
+  function LoopbackServer() {}
+
+  LoopbackServer.prototype.entryPath = normalize(__dirname + '/../../loopback/server/server.js');
+
+  LoopbackServer.prototype.launch = function() {
+    return new Promise((function(_this) {
+      return function(resolve) {
+        return require(_this.entryPath).start(resolve);
+      };
+    })(this));
+  };
+
+  return LoopbackServer;
+
+})();
+
+module.exports = LoopbackServer;
